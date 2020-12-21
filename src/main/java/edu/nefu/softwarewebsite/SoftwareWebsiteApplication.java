ce99f2721918ff0e9703082d01e836662eeb2d4e
wenbinai <208176146@qq.com>
2020-12-21 09:01:25 +0800

    20-12-21 [env] 初始化项目环境(数据库, 端口配置等)

---
+++ src/main/java/edu/nefu/softwarewebsite/SoftwareWebsiteApplication.java
@@ -0,0 +1,13 @@
+package edu.nefu.softwarewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/nefu/softwarewebsite/SoftwareWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nefu.softwarewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
